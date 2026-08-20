c98aa2174228b461762018b4e7efe7e071f3bd2d
spoorty-86 <spoortiyadavcspoorthi@gmail.com>
2026-08-20 12:52:42 +0530

    Remove conflicting root dummy app folders and resolve Notification import error

--- budgetbuddy/finance/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Category, Income, Expense, Budget, SavingsGoal, Notification, Report
-for m in [Category, Income, Expense, Budget, SavingsGoal, Notification, Report]:
-    admin.site.register(m)
--- budgets/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Budget, SavingsGoal
-
-admin.site.register(Budget)
-admin.site.register(SavingsGoal)
